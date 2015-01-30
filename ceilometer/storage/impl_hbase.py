c5c3592c62279cfed40714e624664963708dac47
Stefano Zilli <stefano.zilli@cern.ch>
2015-01-30 08:47:17 +0100

    Introduce separator parameter in hbase
    table_prefix_separator is hardcoded to be an underscore (happybase
    default). This fix adds the possibility to parametrize this value using
    a parameter in ceilometer database configuration. An example is:
    hbase://localhost:9090/?table_prefix=ceilo&table_prefix_separator=.

--- ceilometer/storage/impl_hbase.py
+++ ceilometer/storage/impl_hbase.py
@@ -182,9 +182,12 @@ class Connection(base.Connection):
         """
         LOG.debug(_('connecting to HBase on %(host)s:%(port)s') % (
                   {'host': conf['host'], 'port': conf['port']}))
-        return happybase.ConnectionPool(size=100, host=conf['host'],
-                                        port=conf['port'],
-                                        table_prefix=conf['table_prefix'])
+        return happybase.ConnectionPool(
+            size=100,
+            host=conf['host'],
+            port=conf['port'],
+            table_prefix=conf['table_prefix'],
+            table_prefix_separator=conf['table_prefix_separator'])
 
     @staticmethod
     def _parse_connection_url(url):
@@ -199,6 +202,8 @@ class Connection(base.Connection):
         result = network_utils.urlsplit(url)
         opts['table_prefix'] = urlparse.parse_qs(
             result.query).get('table_prefix', [None])[0]
+        opts['table_prefix_separator'] = urlparse.parse_qs(
+            result.query).get('table_prefix_separator', ['_'])[0]
         opts['dbtype'] = result.scheme
         if ':' in result.netloc:
             opts['host'], port = result.netloc.split(':')
